fb9b4244c174caf9e3020aab102a775222d48ffe
Ron Almog <ron8132@gmail.com>
2025-02-25 14:35:02 -0500

    Add actions component and refactor create region hook for regions

---
+++ src/features/regions/components/actions.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useDeleteRegion } from "../hooks/use-delete-region";
+import { useOpenRegion } from "../hooks/use-open-region";
+
+type Props = { id: string };
+
+const Actions = ({ id }: Props) => {
+  console.log({ id });
+  const deleteMutation = useDeleteRegion();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "You are about to delete this transaction"
+  );
+  const { onOpen } = useOpenRegion();
+
+  const handleDelete = async () => {
+    const ok = await confirm();
+    if (ok) {
+      deleteMutation.mutate();
+    }
+  };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <MoreHorizontal className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={() => onOpen(id)}
+          >
+            <Edit className="size-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            disabled={deleteMutation.isPending}
+            onClick={handleDelete}
+          >
+            <Trash className="size-4 mr-2" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+
+export default Actions;
--- src/features/regions/hooks/use-create-region.ts
+++ src/features/regions/hooks/use-create-region.ts
@@ -1,22 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { RegionValues } from "@/lib/schemas";
 import { createRegion } from "../actions/create-region";
-
-const create = async (values: RegionValues) => {
-  const response = await createRegion(values);
-  return response.data;
-};
 
 export const useCreateRegion = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: create,
+    mutationFn: createRegion,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["regions"] });
-    },
-    onError: (error) => {
-      console.error("Error creating region:", error);
     },
   });
 };
