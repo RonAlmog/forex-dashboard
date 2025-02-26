a3371f413ea55b35d2ddee8a94b81e42d0a76fd5
Ron Almog <ron8132@gmail.com>
2025-02-26 08:16:56 -0500

    add columns for regions table

---
+++ src/features/regions/components/columns.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+import { RegionValues } from "@/lib/schemas";
+import Actions from "./actions";
+
+export const columns: ColumnDef<RegionValues>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Region Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+
+  {
+    id: "actions",
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    header: ({ column }) => {
+      return <Button variant="ghost">Actions</Button>;
+    },
+    cell: ({ row }) => <Actions id={row.original.id} />,
+  },
+];
