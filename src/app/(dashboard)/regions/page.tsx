193297784e1a0d633db4f09605390162773d07c5
Ron Almog <ron8132@gmail.com>
2025-03-02 08:57:47 -0500

    move regions to client

--- src/app/(dashboard)/regions/page.tsx
+++ src/app/(dashboard)/regions/page.tsx
@@ -1,64 +1,10 @@
-"use client";
-
-import { useGetRegions } from "@/features/regions/hooks/use-get-regions";
-import { useNewRegion } from "@/features/regions/hooks/use-new-region";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Loader2, Plus } from "lucide-react";
-import { DataTable } from "@/components/data-table";
-import { columns } from "@/features/regions/components/columns";
-import { useDeleteRegions } from "@/features/regions/hooks/use-delete-regions";
 import { Metadata } from "next";
+import RegionsClient from "./regions-client";
 
 export const metadata: Metadata = {
   title: "Regions",
 };
 
 export default function Regions() {
-  const newRegion = useNewRegion();
-  const { data: regions, isLoading } = useGetRegions();
-  const deleteRegions = useDeleteRegions();
-
-  return (
-    <main>
-      <div className="pb-10">
-        <Card className="border-none drop-shadow-sm mt-1">
-          <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg: justify-between">
-            <CardTitle className="text-xl line-clamp-1">
-              Sales Regions
-            </CardTitle>
-            <div className="flex flex-col lg:flex-row items-center gap-x-2 gap-y-2">
-              <Button
-                size="sm"
-                className="w-full lg:w-auto"
-                onClick={newRegion.onOpen}
-              >
-                <Plus className="size-4 mr-2" />
-                Add New
-              </Button>
-            </div>
-          </CardHeader>
-          <CardContent>
-            {isLoading || !regions ? (
-              <div className="h-10 w-full flex items-center justify-center">
-                <Loader2 className="size-8 text-slate-300 animate-spin" />
-              </div>
-            ) : (
-              <DataTable
-                columns={columns}
-                data={regions}
-                filterKey="name"
-                onDelete={(row) => {
-                  const ids = row.map((r) => r.original.id);
-                  deleteRegions.mutate({ ids });
-                  return null;
-                }}
-                disabled={false}
-              />
-            )}
-          </CardContent>
-        </Card>
-      </div>
-    </main>
-  );
+  return <RegionsClient />;
 }
